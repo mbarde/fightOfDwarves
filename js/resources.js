25b00da63ba7685c5938bf720ea6a03e2b6f625c
Matthias Barde <mbarde@uni-koblenz.de>
2020-02-13 15:00:45 +0100

    make game run without build

---
+++ js/resources.js
@@ -0,0 +1,132 @@
+game.resources = [
+    {
+        "name": "03 Ferrous Rage",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "attack01",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "attack02",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "attack03",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "dead01",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "hit01",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump01",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "menu",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "32x32_font",
+        "type": "image",
+        "src": "data/img/font/32x32_font.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "castle_tileset_part1_0",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part1_0.png"
+    },
+    {
+        "name": "castle_tileset_part2",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part2.png"
+    },
+    {
+        "name": "castle_tileset_part3",
+        "type": "image",
+        "src": "data/img/map/castle_tileset_part3.png"
+    },
+    {
+        "name": "dwarf_blue",
+        "type": "image",
+        "src": "data/img/sprite/dwarf_blue.png"
+    },
+    {
+        "name": "dwarf_brown",
+        "type": "image",
+        "src": "data/img/sprite/dwarf_brown.png"
+    },
+    {
+        "name": "dwarf_green",
+        "type": "image",
+        "src": "data/img/sprite/dwarf_green.png"
+    },
+    {
+        "name": "dwarf_red",
+        "type": "image",
+        "src": "data/img/sprite/dwarf_red.png"
+    },
+    {
+        "name": "dwarves",
+        "type": "image",
+        "src": "data/img/sprite/dwarves.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+        "name": "area02",
+        "type": "tmx",
+        "src": "data/map/area02.tmx"
+    },
+    {
+        "name": "area03",
+        "type": "tmx",
+        "src": "data/map/area03.tmx"
+    },
+    {
+        "name": "area04",
+        "type": "tmx",
+        "src": "data/map/area04.tmx"
+    },
+    {
+        "name": "area05",
+        "type": "tmx",
+        "src": "data/map/area05.tmx"
+    },
+    {
+        "name": "area06",
+        "type": "tmx",
+        "src": "data/map/area06.tmx"
+    },
+    {
+        "name": "area07",
+        "type": "tmx",
+        "src": "data/map/area07.tmx"
+    },
+    {
+        "name": "area08",
+        "type": "tmx",
+        "src": "data/map/area08.tmx"
+    }
+];
